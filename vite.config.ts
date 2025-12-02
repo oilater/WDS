d85ac5ab3c6d134c857626b73ac185a80d36e9af
oilater <oilater@naver.com>
2025-12-02 11:18:34 +0900

    ⚙️ Initialize Project

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import dts from "vite-plugin-dts";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
+export default defineConfig({
+  plugins: [
+    react(),
+    vanillaExtractPlugin(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "WDS",
+      formats: ["es"],
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
